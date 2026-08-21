ddf79cea4b85fd02259eb51497df2ad7c26747e8
Harsh Pathak <harshpathak040@gmail.com>
2026-08-21 23:40:18 +0530

    updated cpp

--- variables_contants.cpp
+++ variables_contants.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 
 int main(){
+    int score;// variable declaration
+    score = 100; // variable initialization
+    
+    int balance = 500; // variable declaration and initialization
+
+
     cout << "learning c++1" << endl;
     cout << "learning c++2" << endl;
     cout << "learning c++3" << endl;
