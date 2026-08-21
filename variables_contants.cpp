5d6bebe607e24caae5f97b05581163bfd0ced0fc
Harsh Pathak <harshpathak040@gmail.com>
2026-08-21 23:30:55 +0530

    updated cpp

--- variables_contants.cpp
+++ variables_contants.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 int main(){
-    cout<<"learning c++1";
+    cout << "learning c++1" << endl;
+    cout << "learning c++2" << endl;
+    cout << "learning c++3" << endl;
+    cout << "learning c++4" << endl;
+    // cout << "learning c++5" << endl;
+    
+    
+    
+    
     return 0;
 }
