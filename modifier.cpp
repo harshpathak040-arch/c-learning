1196bdbc3fad9bb4699a6ea54d097fb6fb2aca86
Harsh Pathak <harshpathak040@gmail.com>
2026-08-22 19:03:15 +0530

    updated cpp

--- modifier.cpp
+++ modifier.cpp
@@ -6,7 +6,7 @@ int main()
    signed bigteapack= 100;
     cout << bigteapack << endl;
 
-    unsigned smallteapack= -50;
+    unsigned smallteapack= 50;
     cout << smallteapack << endl;
 
     long largeTeapack = 1000000000;
