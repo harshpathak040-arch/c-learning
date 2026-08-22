a516c769a282b270136cbd5d0529a6c3474a262b
Harsh Pathak <harshpathak040@gmail.com>
2026-08-22 18:51:28 +0530

    updated cpp

---
+++ Data_type_and_challenges.cpp
@@ -0,0 +1,24 @@
+ #include <iostream>
+
+ using namespace std;
+   
+ int main() 
+ {
+ 
+    int tealeaves = 70;
+
+    float waterteamperature = 99.9;
+
+    double priceOfTea = 40.99;
+
+    char teaQuality = 'A';
+
+    bool isTeaReady = true;
+
+    cout << isTeaReady << endl;
+
+    return 0;
+
+
+ }
+
