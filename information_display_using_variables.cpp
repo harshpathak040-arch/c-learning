8112580bb3b6e5efd23bd7432e86ee4ac9cf2783
Harsh Pathak <harshpathak040@gmail.com>
2026-08-25 22:20:06 +0530

    updated cpp

--- information_display_using_variables.cpp
+++ information_display_using_variables.cpp
@@ -42,7 +42,7 @@ int main(){
     cout<< "These are the prices of the teas:"<< endl;
     cout<< "Tea prices:"<<TeaPrice[0]<<", "<<TeaPrice[1]<<", "<<TeaPrice[2]<<", "<<TeaPrice[3]<<", "<<TeaPrice[4]<<endl;
     cout<<"This is your tea price= TeaType*TeaPrice:"<<endl;
-    getline(cin, userTeaPrice);S
+    getline(cin, userTeaPrice);
 
     return 0;
     
